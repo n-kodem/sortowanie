661b554ec15bb64a3d8300b672fc4916ffe09e40
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-11-26 12:54:13 +0100

    Update Zsort_tester.py

--- Zsort_tester.py
+++ Zsort_tester.py
@@ -1,5 +1,7 @@
+from os import read
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import ttk
 import time
 
 class Window(Frame):
@@ -13,15 +15,28 @@ class Window(Frame):
 
         # create button, link it to clickExitButton() and place it
         exitButton = Button(self, text="Start test", command=self.clickStartTestBtn)
-        exitButton.place(x=0, y=0)
+        exitButton.place(x=70, y=10)
 
         # text with placement
-        text = Label(self, text="Just do it")
-        text.place(x=70,y=90)
+        mintime = Label(self, text="Just do it")
+        mintime.place(x=70,y=40)
+
+        # text with placement
+        avgtime = Label(self, text="Just do it")
+        avgtime.place(x=70,y=60)
+
+        # text with placement
+        maxtime = Label(self, text="Just do it")
+        maxtime.place(x=70,y=80)
 
         numOfLoop = Entry(self, show=None, font=('Arial', 9))
         numOfLoop.place(x=100,y=120,width=50)
 
+        n = StringVar()
+        typeOfSorting = ttk.Combobox(self, width = 27, textvariable = n,state="readonly")
+        typeOfSorting['values'] = ("1","2","3","4")
+        typeOfSorting.current(0)
+        typeOfSorting.place(x=150,y=120)
     def clickStartTestBtn(self):
         filename = fd.askopenfilename()
         if filename != "":
@@ -34,11 +49,8 @@ class Window(Frame):
             for i in range(1000):
                 a += (i**100)
             end = time.time()
-            
-            # difference of start and end variables
-            # gives the time of execution of the
-            # program in between
-            print("The time of execution of above program is :", end-start)
+
+            print("Time:", end-start)
         
 root = Tk()
 app = Window(root)
