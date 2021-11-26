5aacff6c4d369ae246499091ea5cd48a8bbdb996
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-11-26 12:23:57 +0100

    Initial commit

---
+++ Zsort_tester.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import time
+
+with open("inp.txt", "r+") as file1:
+    data=list(map(int,[i.replace("\n","") for i in file1.readlines()]))
+
+print(data)
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)        
+        self.master = master
+
+        # widget can take all window
+        self.pack(fill=BOTH, expand=1)
+
+        # create button, link it to clickExitButton() and place it
+        exitButton = Button(self, text="Start test", command=self.clickStartTestBtn)
+        exitButton.place(x=0, y=0)
+
+        # text with placement
+        text = Label(self, text="Just do it")
+        text.place(x=70,y=90)
+
+        numOfLoop = Entry(self, show=None, font=('Arial', 9))
+        numOfLoop.place(x=100,y=120,width=50)
+
+    def clickStartTestBtn(self):
+        start = time.time()
+        a = 0
+        for i in range(1000):
+            a += (i**100)
+        end = time.time()
+        
+        # difference of start and end variables
+        # gives the time of execution of the
+        # program in between
+        print("The time of execution of above program is :", end-start)
+        
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter button")
+root.geometry("320x200")
+root.mainloop()
---
+++ quicksort.py
@@ -0,0 +1,18 @@
+from warnings import warn
+def quicksort(table:list, left=0, right=None):
+    if(right is None): right=len(table)-1
+    i,j = left,right
+    pivot = table[(left+right)//2]
+    while(i<=j):
+        while(table[i]<pivot):
+            i+=1
+        while(table[j]>pivot):
+            j-=1
+        if(i<=j):
+            table[i],table[j]=table[j],table[i]
+            i+=1
+            j-=1
+    if(left<i): quicksort(table,left,j)
+    if(right>i): quicksort(table,i,right)
+print(quicksort(xd:=[1,4,5,6,9,2,63,6,9]))
+print(xd)
