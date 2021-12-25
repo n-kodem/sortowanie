7ff1d7c8de95cc2b6af985ec757b8d7eb75d495b
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-12-25 20:18:39 +0100

    Better test data and fixed timing

--- Zsort_tester.py
+++ Zsort_tester.py
@@ -1,3 +1,4 @@
+import random
 from os import read
 from heap_sort import *
 from insertion_sort import *
@@ -9,20 +10,23 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 import time
 
-def sorttest(name,repeat,array):
+
+def sorttest(name, repeat, array):
     sort_times = []
 
     for i in range(repeat):
-        start = time.time()
+        start = time.perf_counter()
         quicksort(array.copy())
-        end = time.time()
-        sort_times.append(end-start)
-    
-    return [min(sort_times),sum(sort_times)/len(sort_times),max(sort_times)]
+        end = time.perf_counter()
+        sort_times.append(end - start)
+
+    return [round(min(sort_times), 10), round(sum(sort_times) / len(sort_times), 10), round(max(sort_times), 10)]
+
+
 class Window(Frame):
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)        
+        Frame.__init__(self, master)
         self.master = master
 
         # widget can take all window
@@ -39,53 +43,54 @@ class Window(Frame):
         # text with placement
         global mintime
         mintime = Label(self, text="Just do it")
-        mintime.place(x=70,y=40)
+        mintime.place(x=70, y=40)
 
         # text with placement
         global avgtime
         avgtime = Label(self, text="Just do it")
-        avgtime.place(x=70,y=60)
+        avgtime.place(x=70, y=60)
 
         # text with placement
         global maxtime
         maxtime = Label(self, text="Just do it")
-        maxtime.place(x=70,y=80)
+        maxtime.place(x=70, y=80)
 
         global numOfLoop
-        numOfLoop = Entry(self, show=None, font=('Arial', 9),text="10")
-        numOfLoop.place(x=80,y=120,width=50)
+        numOfLoop = Entry(self, show=None, font=('Arial', 9), text="10")
+        numOfLoop.place(x=80, y=120, width=50)
 
         global values
         global v
         v = StringVar()
-        values = Entry(self, show=None, font=('Arial', 9),text = v)
-        values.place(x=20,y=120,width=50)
+        values = Entry(self, show=None, font=('Arial', 9), text=v)
+        values.place(x=20, y=120, width=50)
 
         n = StringVar()
-        global typeOfSorting 
-        typeOfSorting = ttk.Combobox(self, width = 27, textvariable = n,state="readonly")
-        typeOfSorting['values'] = ("heap_sort","insertion_sort","quicksort","bubble_sort","mergesort")
+        global typeOfSorting
+        typeOfSorting = ttk.Combobox(self, width=27, textvariable=n, state="readonly")
+        typeOfSorting['values'] = ("heap_sort", "insertion_sort", "quicksort", "bubble_sort", "mergesort")
         typeOfSorting.current(0)
-        typeOfSorting.place(x=150,y=120)
-    
+        typeOfSorting.place(x=150, y=120)
+
     def loadTxtBtn(self):
         filename = fd.askopenfilename()
         if filename != "":
             print(filename)
             with open("inp.txt", "r+") as file1:
-                data=list(map(int,[i.replace("\n","") for i in file1.readlines()]))
+                data = list(map(int, [i.replace("\n", "") for i in file1.readlines()]))
             print(data)
-            v.set(" ".join(list(map(str,data))))
+            # with open("inp.txt","w+") as f:
+            #     f.write("\n".join([f'{random.randint(a=0, b=999)}' for i in range(100)]))
+            v.set(" ".join(list(map(str, data))))
+
     def clickStartTestBtn(self):
+        print(list(map(int, v.get().split())))
+        sorts = sorttest(typeOfSorting.get(), int(numOfLoop.get()), list(map(int, v.get().split())))
+        mintime.configure(text=f'Min: {sorts[0]}')
+        avgtime.configure(text=f'Avg: {sorts[1]}')
+        maxtime.configure(text=f'Max: {sorts[2]}')
 
-            print(list(map(int,v.get().split())))
-            sorts = sorttest(typeOfSorting.get(),int(numOfLoop.get()),list(map(int,v.get().split())))
-            mintime.configure(text = str(sorts[0]))
-            avgtime.configure(text = str(sorts[1]))
-            maxtime.configure(text = str(sorts[2]))
-            
 
-        
 root = Tk()
 app = Window(root)
 root.wm_title("Tkinter button")
